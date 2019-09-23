96727fc6711d05420a5c006be13094afdf5c40be
Sotiris Kavvouras <sotos.kavvouras@gmail.com>
2019-09-23 19:18:06 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -12,8 +12,7 @@ class App < Sinatra::Base
     erb :say
   end
   get '/say/:word1/:word2/:word3/:word4/:word5' do
-    <html>
-      <h1><%=params[:word1]+" "+params[:word2]+" "+params[:word3]+" "+params[:word4]+" "+params[:word5]%></h1>
-    </html>
+    @string = params[:word1]+" "+params[:word2]+" "+params[:word3]+" "+params[:word4]+" "+params[:word5]
+    erb :say/show.html
   end
 end
