6fec5bbb9d0b33c238d18a336c7bd33fc39f7c97
Sotiris Kavvouras <sotos.kavvouras@gmail.com>
2019-09-23 18:47:13 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,6 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
+
+  get '/reversename/:name' do
+    name = params[:name].split("%20")
+
+    rname = name[0]
+    rlname = name[1]
+    "<h1><%rname.reverse%> <%rlname.reverse%></h1>" 
+  end
 
 end
